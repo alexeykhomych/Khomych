13007c58611d10205e49980edb6ae73101b8bf55
Alexey Khomych <alexeyfurious@gmail.com>
2016-04-05 16:11:22 +0300

    Homework_lesson1 -> initial commit

---
+++ Homework_lesson1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Homework_lesson1
+//
+//  Created by Admin on 05.04.16.
+//
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
