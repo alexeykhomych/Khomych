f5223291cc3463bcca93c518ae2c1131c5fc4d51
Alexey Khomych <alexeyfurious@gmail.com>
2016-04-19 18:21:57 +0300

    ICDataStructureWithValues.h -> packed structure and created union struct

--- Sources/ICStructType/ICDataStructureWithValues.c
+++ Sources/ICStructType/ICDataStructureWithValues.c
@@ -20,36 +20,34 @@ void ICPrintSizeof() {
 }
 
 void ICArrangementOfElementsInStructure() {
-//    ICStructValue ICSValue = {true, true, false, 5.0, 1, false, false, false, 4, 2.5, 'c', 2, 22, 12};
-    
-    printf("[0]%lu\n"
-           "[1]%lu\n"
-           "[2]%lu\n"
-           "[3]%lu\n"
-           "[4]%lu\n"
-           "[5]%lu\n"
-           "[6]%lu\n"
-           "[7]%lu\n"
-           "[8]%lu\n"
-           "[9]%lu\n"
-           "[10]%lu\n"
-           "[11]%lu\n"
-           "[12]%lu\n"
-           "[13]%lu\n\n",
-           offsetof(ICStructValue, boolValue1),
-           offsetof(ICStructValue, boolValue2),
-           offsetof(ICStructValue, boolValue3),
-           offsetof(ICStructValue, floatValue1),
-           offsetof(ICStructValue, intValue1),
-           offsetof(ICStructValue, boolValue4),
-           offsetof(ICStructValue, boolValue5),
-           offsetof(ICStructValue, boolValue6),
-           offsetof(ICStructValue, shortValue3),
-           offsetof(ICStructValue, doubleValue1),
-           offsetof(ICStructValue, charValue1),
-           offsetof(ICStructValue, shortValue2),
-           offsetof(ICStructValue, longlongValue1),
-           offsetof(ICStructValue, shortValue1));
+//    printf("[0]%lu\n"
+//           "[1]%lu\n"
+//           "[2]%lu\n"
+//           "[3]%lu\n"
+//           "[4]%lu\n"
+//           "[5]%lu\n"
+//           "[6]%lu\n"
+//           "[7]%lu\n"
+//           "[8]%lu\n"
+//           "[9]%lu\n"
+//           "[10]%lu\n"
+//           "[11]%lu\n"
+//           "[12]%lu\n"
+//           "[13]%lu\n\n",
+//           offsetof(ICStructValue, boolValue1),
+//           offsetof(ICStructValue, boolValue2),
+//           offsetof(ICStructValue, boolValue3),
+//           offsetof(ICStructValue, floatValue1),
+//           offsetof(ICStructValue, intValue1),
+//           offsetof(ICStructValue, boolValue4),
+//           offsetof(ICStructValue, boolValue5),
+//           offsetof(ICStructValue, boolValue6),
+//           offsetof(ICStructValue, shortValue3),
+//           offsetof(ICStructValue, doubleValue1),
+//           offsetof(ICStructValue, charValue1),
+//           offsetof(ICStructValue, shortValue2),
+//           offsetof(ICStructValue, longlongValue1),
+//           offsetof(ICStructValue, shortValue1));
 }
 
 void ICByteValueOutputTest() {
