12d765e8394754611b88e08fa015d9d415ff7ed5
Alexey Khomych <alexeyfurious@gmail.com>
2016-05-30 01:08:48 +0300

    AKILinkedListPrivate.h -> added private declaration methods

--- Project2Human/AKIHuman/Source/AKILinkedList/AKILinkedListPrivate.h
+++ Project2Human/AKIHuman/Source/AKILinkedList/AKILinkedListPrivate.h
@@ -9,5 +9,15 @@
 #ifndef AKILinkedListPrivate_h
 #define AKILinkedListPrivate_h
 
+typedef struct AKILinkedList AKILinkedList; 
+
+extern
+void AKILinkedListSetHead(AKILinkedList *list, AKILinkedListNode *node);
+
+extern
+AKILinkedListNode *AKILinkedListGetHead(AKILinkedList *list);
+
+extern
+void AKILinkedListSetMutationsCount(AKILinkedList *list, uint64_t count);
 
 #endif /* AKILinkedListPrivate_h */
